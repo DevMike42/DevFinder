f408132978eaef0e88c8a42f07f4ddc79e57ccd3
Mike Rivera <michaelstephenrivera@gmail.com>
2020-05-19 08:22:44 -0700

    Added User component and worked on displaying info for a single user to the UI

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
+import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import About from './components/pages/About';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 
@@ -12,6 +13,7 @@ import Alert from './components/layout/Alert';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -28,6 +30,7 @@ class App extends Component {
   //   });
   // };
 
+  // Search Github users
   searchUsers = async (text) => {
     this.setState({ loading: true });
 
@@ -35,6 +38,18 @@ class App extends Component {
 
     this.setState({
       users: res.data.items,
+      loading: false
+    });
+  };
+
+  // Get singer Github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({
+      user: res.data,
       loading: false
     });
   };
@@ -50,7 +65,7 @@ class App extends Component {
 
   render() {
 
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
@@ -71,6 +86,9 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props} getUser={this.getUser} user={user} loading={loading} />
+              )} />
             </Switch>
           </div>
         </div>
